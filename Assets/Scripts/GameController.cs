dbf04e5ccbee94f9975b68ec8bfb45ba0c2fe31a
trinarr <breaker1994@mail.ru>
2022-12-31 01:15:20 +0300

    Fix chat restarting

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+    public void ExitToMenu()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("Scene disable");
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("Level Loaded "+ scene.name+" "+ mode);
+    }
+}
