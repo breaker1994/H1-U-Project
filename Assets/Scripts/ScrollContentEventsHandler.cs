dd4747134b2d883c1597b1b7dcccf933628a0b7d
trinarr <breaker1994@mail.ru>
2023-01-06 00:40:37 +0300

    Add custom block for main character speech

--- Assets/Scripts/ScrollContentEventsHandler.cs
+++ Assets/Scripts/ScrollContentEventsHandler.cs
@@ -4,7 +4,6 @@ using PixelCrushers.DialogueSystem.Extras;
 using UnityEngine.Events;
 
 public class ScrollContentEventsHandler : MonoBehaviour, IPointerClickHandler {
-    public TextlineDialogueUI textlineDialogueUI = null;
     public UnityEvent onClick = new UnityEvent();
 
     public void OnPointerClick(PointerEventData data)
