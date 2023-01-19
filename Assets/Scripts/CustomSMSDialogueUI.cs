1a360016d4fc8e53be46ace84422110b3158b842
trinarr <breaker1994@mail.ru>
2023-01-19 16:09:31 +0300

    Remove test nodes

--- Assets/Scripts/CustomSMSDialogueUI.cs
+++ Assets/Scripts/CustomSMSDialogueUI.cs
@@ -6,7 +6,7 @@ public class CustomSMSDialogueUI : SMSDialogueUI
     protected override StandardUISubtitlePanel GetTemplate(Subtitle subtitle)
     {
         var panelNumber = DialogueActor.GetSubtitlePanelNumber(subtitle.speakerInfo.transform);
-        return (panelNumber == SubtitlePanelNumber.Default) 
+        return (panelNumber == SubtitlePanelNumber.Default)
             ? base.GetTemplate(subtitle)
             : conversationUIElements.subtitlePanels[PanelNumberUtility.GetSubtitlePanelIndex(panelNumber)];
     }
